e22bb6cf21b55fb434206e9406f3eb1b4b0cd7f6
Yan Jiang <yan.jiang@canonical.com>
2024-11-13 10:28:52 +0000

    Revert changes for lldp docker packaging

--- dockers/docker-lldp/docker-lldp-init.sh
+++ dockers/docker-lldp/docker-lldp-init.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 #Generate supervisord.conf based on device metadata
-
-cp -r ${SNAP}/etc/supervisor/ ${SNAP_DATA}/etc/
-
-sonic-cfggen -d -a "{\"namespace_id\":\"$NAMESPACE_ID\"}" -t ${SNAP}/usr/share/sonic/templates/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
-exec ${SNAP}/usr/local/bin/supervisord
+mkdir -p /etc/supervisor/conf.d/
+sonic-cfggen -d -a "{\"namespace_id\":\"$NAMESPACE_ID\"}" -t /usr/share/sonic/templates/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
+exec /usr/local/bin/supervisord
--- dockers/docker-lldp/snap/snap-lldp-init.sh
+++ dockers/docker-lldp/snap/snap-lldp-init.sh
@@ -2,6 +2,7 @@
 #Generate supervisord.conf based on device metadata
 
 cp -r ${SNAP}/etc/supervisor/ ${SNAP_DATA}/etc/
+mkdir -p /var/run/snap.lldp/
 
 sonic-cfggen -d -a "{\"namespace_id\":\"$NAMESPACE_ID\"}" -t ${SNAP}/usr/share/sonic/templates/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
 exec ${SNAP}/usr/local/bin/supervisord
