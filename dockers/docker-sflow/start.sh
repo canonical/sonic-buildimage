a967a08470457b6bcbbe0ec4ad544a92ea817bbe
jy5275 <yanjiangpku@gmail.com>
2025-12-08 12:12:07 +0000

    Remove rsyslog dependency

--- dockers/docker-sflow/start.sh
+++ dockers/docker-sflow/start.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+LAYER_FILE="/tmp/syslog-layer.yaml"
+
+echo "
+log-targets:
+  host-syslog:
+    override: replace
+    type: syslog
+    location: udp://127.0.0.1:514/
+    services: [all]
+" > $LAYER_FILE
+
+echo "File created: $LAYER_FILE"
+echo "Service Dependency: $DEPENDENCY"
+echo "---"
+pebble add syslog-layer --combine $LAYER_FILE
+pebble replan
+
 TZ=$(cat /etc/timezone)
 rm -rf /etc/localtime
 ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
