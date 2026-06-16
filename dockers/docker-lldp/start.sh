934ed9b9e0cf9caab8b2df82d11941c3a3758056
jy5275 <yanjiangpku@gmail.com>
2026-06-16 07:42:35 +0000

    Revert "Tidy rockcraft.yaml"
    This reverts commit e1840064888a385ee9763a6e8d8e05bada35dc13.

--- dockers/docker-lldp/start.sh
+++ dockers/docker-lldp/start.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-LAYER_FILE="/usr/share/sonic/templates/syslog-layer.yaml"
+LAYER_FILE="/tmp/syslog-layer.yaml"
+
+echo "
+log-targets:
+  host-syslog:
+    override: replace
+    type: syslog
+    location: udp://127.0.0.1:514/
+    services: [all]
+" > $LAYER_FILE
+
+echo "File created: $LAYER_FILE"
+echo "Service Dependency: $DEPENDENCY"
+echo "---"
 pebble add syslog-layer --combine $LAYER_FILE
 pebble replan
 
@@ -32,6 +45,3 @@ rm -f /var/run/lldpd.socket
 TZ=$(cat /etc/timezone)
 rm -rf /etc/localtime
 ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
-
-pebble start lldpd
-pebble start waitfor_lldp_ready
--- dockers/docker-lldp/waitfor_lldp_ready.sh
+++ dockers/docker-lldp/waitfor_lldp_ready.sh
@@ -22,6 +22,3 @@ do
     lldpcli -u /var/run/lldpd.socket -c /etc/lldpd.conf -c /etc/lldpd.d resume > /dev/null && break
     sleep 1
 done
-
-pebble start lldp-syncd
-pebble start lldpmgrd
