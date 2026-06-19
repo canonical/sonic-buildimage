d7fbb76aec90fcaf6e82d0cc7e282f22db7b97f5
jy5275 <yanjiangpku@gmail.com>
2026-06-19 08:56:53 +0000

    Revert "Change pebble service start order control"
    This reverts commit 49449cf9acc282906e90bfb5a75a141f4503181a.

--- dockers/docker-snmp/start.sh
+++ dockers/docker-snmp/start.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-LAYER_FILE="/usr/share/sonic/templates/syslog-layer.yaml"
+LAYER_FILE="/tmp/syslog-layer.yaml"
+
+echo "
+log-targets:
+  host-syslog:
+    override: replace
+    type: syslog
+    location: udp://127.0.0.1:514/
+    services: [all]
+" > $LAYER_FILE
+
+echo "File created: $LAYER_FILE"
+echo "Service Dependency: $DEPENDENCY"
+echo "---"
 pebble add syslog-layer --combine $LAYER_FILE
 pebble replan
 
@@ -34,6 +47,3 @@ echo "# Config files managed by sonic-config-engine" > /var/sonic/config_status
 TZ=$(cat /etc/timezone)
 rm -rf /etc/localtime
 ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
-
-pebble start snmpd
-pebble start snmp-subagent
--- dockers/docker-teamd/start.sh
+++ dockers/docker-teamd/start.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-LAYER_FILE="/usr/share/sonic/templates/syslog-layer.yaml"
+LAYER_FILE="/tmp/syslog-layer.yaml"
+
+echo "
+log-targets:
+  host-syslog:
+    override: replace
+    type: syslog
+    location: udp://127.0.0.1:514/
+    services: [all]
+" > $LAYER_FILE
+
+echo "File created: $LAYER_FILE"
+echo "Service Dependency: $DEPENDENCY"
+echo "---"
 pebble add syslog-layer --combine $LAYER_FILE
 pebble replan
 
@@ -11,7 +24,3 @@ mkdir -p /var/warmboot/teamd
 TZ=$(cat /etc/timezone)
 rm -rf /etc/localtime
 ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
-
-pebble start teammgrd
-pebble start tlm_teamd
-pebble start teamsyncd
