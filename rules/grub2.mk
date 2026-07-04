9eb1920d96e710ab9357e5fc31f814190c27b4c2
Sheldon Qi <sheldon.qi@canonical.com>
2026-07-04 08:52:46 +0000

    build: add src:grub2-unsigned build (Ubuntu grub2 split, produces efi-amd64-bin)
    Ubuntu resolute splits grub2 into src:grub2 (grub2-common/grub-pc etc.) and
    src:grub2-unsigned (grub-efi-amd64 / -bin / -dbg / -unsigned). Debian trixie
    had a single src:grub2 producing all. Verified: building grub2-unsigned dsc
    (DEB_SOURCE=grub2-unsigned -> SB_SUBMIT=yes -> ONLY_BUILD=-pgrub-efi-amd64-bin)
    produces the full grub-efi-amd64 series.
    
    - rules/grub2.mk: move GRUB_EFI_AMD64 / GRUB_EFI_AMD64_BIN out of grub2's
      derived (grub2 excludes them) into a new grub2-unsigned build (SRC_PATH=
      src/grub2-unsigned, version 2.14-2ubuntu1, depends on grub2-common).
    - src/grub2/Makefile: drop grub-efi-amd64* from DERIVED_TARGETS.
    - src/grub2-unsigned/Makefile: dget grub2-unsigned dsc + cp-al overlayfs patch
      (reuses src/grub2/patch-overlayfs-ln.sh) + dpkg-buildpackage.

--- rules/grub2.mk
+++ rules/grub2.mk
@@ -20,18 +20,33 @@ $(eval $(call add_derived_package,$(GRUB2_COMMON),$(GRUB_EFI)))
 ifeq ($(CONFIGURED_ARCH),amd64)
 GRUB_PC_BIN = grub-pc-bin_$(GRUB2_VERSION)_$(CONFIGURED_ARCH).deb
 $(eval $(call add_derived_package,$(GRUB2_COMMON),$(GRUB_PC_BIN)))
-
-GRUB_EFI_AMD64 = grub-efi-amd64_$(GRUB2_VERSION)_$(CONFIGURED_ARCH).deb
-$(eval $(call add_derived_package,$(GRUB2_COMMON),$(GRUB_EFI_AMD64)))
-
-GRUB_EFI_AMD64_BIN = grub-efi-amd64-bin_$(GRUB2_VERSION)_$(CONFIGURED_ARCH).deb
-$(eval $(call add_derived_package,$(GRUB2_COMMON),$(GRUB_EFI_AMD64_BIN)))
 else ifeq ($(CONFIGURED_ARCH),arm64)
-GRUB_EFI_ARM64 = grub-efi-arm64_$(GRUB2_VERSION)_$(CONFIGURED_ARCH).deb
-$(eval $(call add_derived_package,$(GRUB2_COMMON),$(GRUB_EFI_ARM64)))
+endif
 
-GRUB_EFI_ARM64_BIN = grub-efi-arm64-bin_$(GRUB2_VERSION)_$(CONFIGURED_ARCH).deb
-$(eval $(call add_derived_package,$(GRUB2_COMMON),$(GRUB_EFI_ARM64_BIN)))
+# resolute: Ubuntu splits grub2 into src:grub2 (above) and src:grub2-unsigned
+# (below). src:grub2 builds grub2-common/grub-pc etc. but EXCLUDES
+# grub-efi-amd64-bin/unsigned/dbg (debian/rules: SB_SUBMIT=no when
+# DEB_SOURCE=grub2). Those come from src:grub2-unsigned (DEB_SOURCE=grub2-unsigned
+# -> SB_SUBMIT=yes -> ONLY_BUILD=-pgrub-efi-amd64-bin...). Debian trixie had a
+# single src:grub2 producing all; Ubuntu resolute split them, so we build both.
+GRUB2_UNSIGNED_VERSION = 2.14-2ubuntu1
+
+ifeq ($(CONFIGURED_ARCH),amd64)
+GRUB_EFI_AMD64 = grub-efi-amd64_$(GRUB2_UNSIGNED_VERSION)_$(CONFIGURED_ARCH).deb
+$(GRUB_EFI_AMD64)_SRC_PATH = $(SRC_PATH)/grub2-unsigned
+$(GRUB_EFI_AMD64)_DEPENDS += $(GRUB2_COMMON)
+SONIC_MAKE_DEBS += $(GRUB_EFI_AMD64)
+
+GRUB_EFI_AMD64_BIN = grub-efi-amd64-bin_$(GRUB2_UNSIGNED_VERSION)_$(CONFIGURED_ARCH).deb
+$(eval $(call add_derived_package,$(GRUB_EFI_AMD64),$(GRUB_EFI_AMD64_BIN)))
+else ifeq ($(CONFIGURED_ARCH),arm64)
+GRUB_EFI_ARM64 = grub-efi-arm64_$(GRUB2_UNSIGNED_VERSION)_$(CONFIGURED_ARCH).deb
+$(GRUB_EFI_ARM64)_SRC_PATH = $(SRC_PATH)/grub2-unsigned
+$(GRUB_EFI_ARM64)_DEPENDS += $(GRUB2_COMMON)
+SONIC_MAKE_DEBS += $(GRUB_EFI_ARM64)
+
+GRUB_EFI_ARM64_BIN = grub-efi-arm64-bin_$(GRUB2_UNSIGNED_VERSION)_$(CONFIGURED_ARCH).deb
+$(eval $(call add_derived_package,$(GRUB_EFI_ARM64),$(GRUB_EFI_ARM64_BIN)))
 endif
 
 # The .c, .cpp, .h & .hpp files under src/{$DBG_SRC_ARCHIVE list}
