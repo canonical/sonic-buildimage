305713466431ff53c7bd2adcbed29c46d99a11ad
jy5275 <yanjiangpku@gmail.com>
2026-07-23 17:06:14 +0000

    build: migrate docker-eventd to rockcraft+pebble
    - rockcraft.yaml: flatten inheritance chain with pebble services and
      jinjanator for j2 template rendering in build env
    - start.sh: pebble/supervisord coexistence detection
    - build_rocks.sh: add docker-eventd to rocklist

--- dockers/docker-eventd/start.sh
+++ dockers/docker-eventd/start.sh
@@ -3,3 +3,11 @@
 if [ "${RUNTIME_OWNER}" == "" ]; then
     RUNTIME_OWNER="kube"
 fi
+
+if pgrep -x pebble > /dev/null 2>&1; then
+    LAYER_FILE="/usr/share/sonic/templates/syslog-layer.yaml"
+    pebble add syslog-layer --combine $LAYER_FILE
+    pebble replan
+    pebble start eventd
+    pebble start eventdb
+fi
