461223bb514ded55f61b62803d124d8d0480b3e6
Sheldon Qi <sheldon.qi@canonical.com>
2026-07-25 04:26:13 +0000

    de-fork iproute2: add Ubuntu (resolute) source alongside Debian (trixie)

--- rules/iproute2.mk
+++ rules/iproute2.mk
@@ -1,14 +1,22 @@
 # iproute2 - patched for EVPN Multihoming protocol field support
 
-ifeq ($(BLDENV),trixie)
+ifneq ($(filter $(BLDENV), trixie resolute),)
 
+ifeq ($(BLDENV), trixie)
 IPROUTE2_VERSION_BASE = 6.15.0
-IPROUTE2_DEBIAN_VERSION = $(IPROUTE2_VERSION_BASE)-1
-IPROUTE2_VERSION_FULL = $(IPROUTE2_DEBIAN_VERSION)+sonic.0
+IPROUTE2_VERSION = $(IPROUTE2_VERSION_BASE)-1
+IPROUTE2_DSC_URL = https://deb.debian.org/debian/pool/main/i/iproute2/iproute2_$(IPROUTE2_VERSION).dsc
+else
+IPROUTE2_VERSION_BASE = 6.19.0
+IPROUTE2_VERSION = $(IPROUTE2_VERSION_BASE)-1ubuntu1
+IPROUTE2_DSC_URL = http://archive.ubuntu.com/ubuntu/pool/main/i/iproute2/iproute2_$(IPROUTE2_VERSION).dsc
+endif
+IPROUTE2_VERSION_FULL = $(IPROUTE2_VERSION)+sonic.0
 
 export IPROUTE2_VERSION_BASE
-export IPROUTE2_DEBIAN_VERSION
+export IPROUTE2_VERSION
 export IPROUTE2_VERSION_FULL
+export IPROUTE2_DSC_URL
 export IPROUTE2
 export IPROUTE2_DBG
 
@@ -21,4 +29,4 @@ $(IPROUTE2_DBG)_DEPENDS += $(IPROUTE2)
 $(IPROUTE2_DBG)_RDEPENDS += $(IPROUTE2)
 $(eval $(call add_derived_package,$(IPROUTE2),$(IPROUTE2_DBG)))
 
-endif  # trixie
+endif
